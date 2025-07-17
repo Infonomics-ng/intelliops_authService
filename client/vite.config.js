31479ed06126eab33ef23884d1094cb57c7936e8
mostcharming <mostcharming920@yahoo.com>
2025-07-17 09:33:27 +0100

    Updates to the app launcher

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,14 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import postcss from './postcss.config.js';
-
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import postcss from "./postcss.config.js";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true, // or use '0.0.0.0'
+  },
   plugins: [react()],
   css: {
     postcss,
   },
-})
+});
--- server/index.js
+++ server/index.js
@@ -24,6 +24,8 @@ const whitelist = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
   "http://localhost:5174",
+  "http://192.168.1.187:5174",
+  "http://192.168.1.187:5173",
 ];
 
 const corsOptions = {
