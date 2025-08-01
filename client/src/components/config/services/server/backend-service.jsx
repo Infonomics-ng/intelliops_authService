ab671837e28604f2baf0c11b159948b0bc905c8f
mostcharming <mostcharming920@yahoo.com>
2025-08-01 19:08:17 +0100

    Update to the fall back

--- client/src/components/config/services/server/backend-service.jsx
+++ client/src/components/config/services/server/backend-service.jsx
@@ -1,79 +1,79 @@
-import axios from 'axios'
-import { useMutation } from 'react-query'
-import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import { toast } from 'react-toastify'
+import axios from "axios";
+import { useMutation } from "react-query";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
-import { logoutR } from '../../../../store/reducers/authReducer'
-import { useAuth } from '../../hooks/context/authProvider'
-import BaseURL from './backendUrl'
+import { logoutR } from "../../../../store/reducers/authReducer";
+import { useAuth } from "../../hooks/context/authProvider";
+import BaseURL from "./backendUrl";
 
-const baseURL = `${BaseURL.BACKEND_SERVER_URL}/api/v2`
+const baseURL = `${BaseURL.BACKEND_SERVER_URL}/api/v2`;
 
 const useBackendService = (endpoint, method, options) => {
-  const { token } = useAuth()
+  const { token } = useAuth();
 
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const url = baseURL + endpoint
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const url = baseURL + endpoint;
 
   const backendService = async (payload, config = {}) => {
-    const lowerCaseMethod = method.toLowerCase()
+    const lowerCaseMethod = method.toLowerCase();
 
     const headers = {
       ...config.headers,
-      Authorization: token ? `Bearer ${token}` : undefined
-    }
+      Authorization: token ? `Bearer ${token}` : undefined,
+    };
 
     if (payload instanceof FormData) {
-      headers['Content-Type'] = 'multipart/form-data'
+      headers["Content-Type"] = "multipart/form-data";
     }
 
-    config.headers = headers
+    config.headers = headers;
 
     switch (lowerCaseMethod) {
-      case 'get':
+      case "get":
         return axios
           .get(url, { params: payload, ...config })
-          .then((res) => res.data)
-      case 'post':
-        return axios.post(url, payload, config).then((res) => res.data)
-      case 'put':
-        return axios.put(url, payload, config).then((res) => res.data)
-      case 'delete':
+          .then((res) => res.data);
+      case "post":
+        return axios.post(url, payload, config).then((res) => res.data);
+      case "put":
+        return axios.put(url, payload, config).then((res) => res.data);
+      case "delete":
         return axios
           .delete(url, { data: payload, ...config })
-          .then((res) => res.data)
+          .then((res) => res.data);
       default:
-        throw new Error(`Unsupported method: ${method}`)
+        throw new Error(`Unsupported method: ${method}`);
     }
-  }
+  };
 
   const enhancedOptions = {
     ...options,
     onError: (error) => {
       if (!navigator.onLine) {
         toast.error(
-          'No internet connection. Please check your network and try again.'
-        )
-        return
+          "No internet connection. Please check your network and try again."
+        );
+        return;
       }
 
-      const errorMessage = error.response?.data?.message?.toLowerCase()
-      if (errorMessage && errorMessage.includes('unauthorized')) {
+      const errorMessage = error.response?.data?.message?.toLowerCase();
+      if (errorMessage && errorMessage.includes("unauthorized")) {
         // toast.error('Session expired. Please log in again.')
-        dispatch(logoutR())
-        navigate('/login')
-        return
+        dispatch(logoutR());
+        navigate("/login");
+        return;
       }
 
       if (options.onError) {
-        options.onError(error)
+        options.onError(error);
       }
-    }
-  }
+    },
+  };
 
-  return useMutation(backendService, enhancedOptions)
-}
+  return useMutation(backendService, enhancedOptions);
+};
 
-export default useBackendService
+export default useBackendService;
--- client/src/components/pages/menu/index.jsx
+++ client/src/components/pages/menu/index.jsx
@@ -1,41 +1,41 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import { logoutR } from '../../../store/reducers/authReducer'
-import { useAuth } from '../../config/hooks/context/authProvider'
-import mtn from '../../resources/images/mtn.png'
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logoutR } from "../../../store/reducers/authReducer";
+import { useAuth } from "../../config/hooks/context/authProvider";
+import mtn from "../../resources/images/mtn.png";
 
 const Menu = () => {
-  const { user, token } = useAuth()
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
+  const { user, token } = useAuth();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const userData = {
     user: user,
-    token: token
-  }
+    token: token,
+  };
 
   useEffect(() => {
     if (user.firstTime === 1) {
-      navigate('/firsttime')
+      navigate("/firsttime");
     } else if (!user.userModules || user.userModules.length === 0) {
-      navigate('/no_module')
+      navigate("/no_module");
     } else if (user.userModules.length === 1) {
-      const moduleUrl = user.userModules[0]?.url
+      const moduleUrl = user.userModules[0]?.url;
       if (moduleUrl) {
-        window.location.href = moduleUrl
+        window.location.href = moduleUrl;
       }
     }
-  }, [user, navigate])
+  }, [user, navigate]);
 
   const handleLogout = () => {
-    dispatch(logoutR())
-    navigate('/login')
-  }
+    dispatch(logoutR());
+    navigate("/login");
+  };
 
   const postUserDataToModule = (url, userData) => {
-    const encodedData = encodeURIComponent(JSON.stringify(userData))
-    window.location.href = `${url}?UA=${encodedData}`
-  }
+    const encodedData = encodeURIComponent(JSON.stringify(userData));
+    window.location.href = `${url}?UA=${encodedData}`;
+  };
 
   if (user.userModules.length > 1) {
     return (
@@ -43,65 +43,65 @@ const Menu = () => {
         <div
           style={{
             backgroundImage: `url(${mtn})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat'
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
           }}
-          className='min-h-screen py-12 bg-gray-100'
+          className="min-h-screen py-12 bg-gray-100"
         >
           <section>
-            <div className='flex justify-end p-4'>
+            <div className="flex justify-end p-4">
               <button
                 onClick={handleLogout}
-                className='px-4 py-2 font-bold text-white bg-red-600 rounded hover:bg-red-700'
+                className="px-4 py-2 font-bold text-white bg-red-600 rounded hover:bg-red-700"
               >
                 Logout
               </button>
             </div>
-            <h1 className='text-4xl font-extrabold text-center text-white'>
+            <h1 className="text-4xl font-extrabold text-center text-white">
               Welcome to Intelliops
             </h1>
             <br />
-            <h2 className='text-4xl font-extrabold text-center text-white'>
+            <h2 className="text-4xl font-extrabold text-center text-white">
               Please select a Module
             </h2>
             <div
-              style={{ display: 'flex', flexWrap: 'wrap' }}
-              className='content-center lg:flex lg:justify-center lg:items-center'
+              style={{ display: "flex", flexWrap: "wrap" }}
+              className="content-center lg:flex lg:justify-center lg:items-center"
             >
               {user.userModules.map((module, index) => (
                 <div
                   key={module.modulecd}
-                  className='shadow cursor-pointer'
+                  className="shadow cursor-pointer"
                   onClick={() => postUserDataToModule(module.url, userData)}
                 >
-                  <div className='flex justify-center pt-10 m-auto lg:w-1/4 lg:mx-6 lg:my-8'>
-                    <div className='relative w-64 h-48'>
-                      <div className='absolute top-0 left-0 flex items-center w-64 h-40 mt-6 ml-6 bg-white border-8 border-gray-700 border-solid rounded-lg'>
-                        <div className='w-1/3 h-40'></div>
-                        <div className='w-2/3 h-32 pr-4'>
-                          <h3 className='pt-1 text-xl font-semibold text-gray-700'>
+                  <div className="flex justify-center pt-10 m-auto lg:w-1/4 lg:mx-6 lg:my-8">
+                    <div className="relative w-64 h-48">
+                      <div className="absolute top-0 left-0 flex items-center w-64 h-40 mt-6 ml-6 bg-white border-8 border-gray-700 border-solid rounded-lg">
+                        <div className="w-1/3 h-40"></div>
+                        <div className="w-2/3 h-32 pr-4">
+                          <h3 className="pt-1 text-xl font-semibold text-gray-700">
                             {module.module_name}
                           </h3>
                         </div>
                       </div>
-                      <div className='absolute top-0 left-0 z-20 w-12 h-12 mt-6 ml-6 bg-white rounded-full'>
+                      <div className="absolute top-0 left-0 z-20 w-12 h-12 mt-6 ml-6 bg-white rounded-full">
                         <svg
-                          className='mt-2 ml-2'
-                          xmlns='http://www.w3.org/2000/svg'
-                          viewBox='0 0 24 24'
-                          fill='#FFCB05'
-                          width='32px'
-                          height='32px'
+                          className="mt-2 ml-2"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="#FFCB05"
+                          width="32px"
+                          height="32px"
                         >
-                          <path d='M0 0h24v24H0z' fill='none' />
-                          <path d='M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-5 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z' />
+                          <path d="M0 0h24v24H0z" fill="none" />
+                          <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-5 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z" />
                         </svg>
                       </div>
-                      <div className='absolute top-0 left-0 z-10 w-24 h-40 py-20 text-5xl font-bold text-center text-white bg-[#FFCB05] rounded-lg'>
+                      <div className="absolute top-0 left-0 z-10 w-24 h-40 py-20 text-5xl font-bold text-center text-white bg-[#FFCB05] rounded-lg">
                         {index}
                       </div>
-                      <div className='absolute top-0 left-0 z-30 w-24 h-2 mt-40 ml-48 bg-[#FFCB05]'></div>
+                      <div className="absolute top-0 left-0 z-30 w-24 h-2 mt-40 ml-48 bg-[#FFCB05]"></div>
                     </div>
                   </div>
                 </div>
@@ -110,10 +110,10 @@ const Menu = () => {
           </section>
         </div>
       </main>
-    )
+    );
   }
 
-  return <div>Login</div>
-}
+  return <div>Login</div>;
+};
 
-export default Menu
+export default Menu;
